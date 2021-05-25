19f23762c1b30ba7ba39bc8b708daf90d5b2ae43
akapu <alexkap544@gmail.com>
2021-05-26 00:44:15 +1000

    initial commit

---
+++ color.py
@@ -0,0 +1,56 @@
+""" Fixes most of plain setColor function's arguments
+    (just integer numbers, not variables or function calls) """
+
+LEFT = 'love.graphics.setColor('
+RIGHT = ')'
+DELIMITER = ','
+
+def fix_color(filename):
+    fixed_file = []
+
+    with open(filename) as file:
+        for line in file:
+            line = fix_line(line)
+            fixed_file.append(line)
+            
+def fix_line(line):
+    first = line.find(LEFT)
+
+    if first == -1:
+        return line
+
+    last = line.find(RIGHT, first)
+
+    arguments = line[first:last].split(DELIMITER)
+
+    digit_args_counter = 0
+    for arg in arguments:
+        if not arg.strip().isdigit():
+            break
+        digit_args_counter += 1
+
+    arguments_fixed = []
+    for i in range(digit_args_counter):
+        arguments_fixed.append(fix_number(arguments[i]))
+
+
+    return line
+
+def fix_number(number):
+    return number
+
+def main():
+    """Entry point"""
+    #fix line testing
+    NO_FIX_LINE = "love.graphics.draw(gTextures['background'], backgroundX, 0)"
+    assert fix_line(NO_FIX_LINE) == NO_FIX_LINE
+    
+    SIMPLE_LINE = 'love.graphics.setColor(255, 255, 255, 255)'
+    FIXED_SIMPLE_LINE = 'love.graphics.setColor(1.0, 1.0, 1.0, 1.0)'
+    assert fix_line(SIMPLE_LINE) == FIXED_SIMPLE_LINE
+
+    #fix_number testing
+    assert fix_number('255') == ' 1.0'
+
+if __name__ == '__main__':
+    main()
---
+++ color_test.py
@@ -0,0 +1,23 @@
+import color
+
+class TestLine:
+    def test_no_fix(self):
+        no_fix_lines = ("love.graphics.draw(gTextures['background'], "
+                "backgroundX, 0)",
+                "love.graphics.setColor(some_color, 255, 255, 255)")
+
+        for line in no_fix_lines:
+            assert color.fix_line(line) == line
+
+    def test_simple_fix(self):
+        simple_line = "love.graphics.setColor(255, 255, 255, 255)"
+        fixed_simple_line = "love.graphics.setColor(1.0, 1.0, 1.0, 1.0)"
+
+        assert color.fix_line(simple_line) == fixed_simple_line
+
+    def test_comlex_fix(self):
+        complex_line = "love.graphics.setColor(255, 255, some_color, 255)"
+        fixed_complex_line = "love.graphics.setColor(1.0, 1.0, "
+        "some_color, 255)"
+
+
